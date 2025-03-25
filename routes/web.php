42583cf2cbc56c37417a68f289c6f626642b625d
IzzaCan <putranur.ir26@gmail.com>
2025-03-25 21:58:24 +0700

    Pertemuan 3

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,54 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
 
-Route::get('/products', function () {
-    return view('products');
-});
+    Route::get('/', function(){
+    $title = "Homepage";
+    return view('web.homepage',['title'=>$title]);
+   });
+   Route::get('products', function(){
+    $title = "Products";
+    return view('web.products',['title'=>$title]);
+   });
+   Route::get('product/{slug}', function($slug){
+    $title = "Single Product";
+    return view('web.single_product',['title'=>$title,'slug'=>$slug]);
+   });
+   Route::get('categories', function(){
+    $title = "Categories";
+    return view('web.categories',['title'=>$title]);
+   });
+   Route::get('category/{slug}', function($slug){
+    $title = "Single Category";
+    return view('web.single_category',['title'=>$title,'slug'=>$slug]);
+   });
+   Route::get('cart', function(){
+    $title = "Cart";
+    return view('web.cart',['title'=>$title]);
+   });
+   Route::get('checkout', function(){
+    $title = "Checkout";
+    return view('web.checkout',['title'=>$title]);
+   });
 
-Route::get('/products/{id}', function ($id) {
-    return view('product-detail', ['id' => $id]);
-});
 
-Route::get('/cart', function () {
-    return view('cart');
-});
 
-Route::get('/checkout', function () {
-    return view('checkout');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+// Route::get('/products', function () {
+//     return view('products');
+// });
+
+// Route::get('/products/{id}', function ($id) {
+//     return view('product-detail', ['id' => $id]);
+// });
+
+// Route::get('/cart', function () {
+//     return view('cart');
+// });
+
+// Route::get('/checkout', function () {
+//     return view('checkout');
+// });
